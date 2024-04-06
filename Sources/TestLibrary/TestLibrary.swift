3a9a6e2baa913c2d9e86c6df291d58f22bff6d72
kudit <support@kudit.com>
2024-04-06 14:52:55 -0400

    v1.0

--- Sources/TestLibrary/TestLibrary.swift
+++ Sources/TestLibrary/TestLibrary.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+extension String {
+    static var testLibrary: String {
+        "The test library \(String(describing: Bundle.module)) exists!"
+    }
+}
