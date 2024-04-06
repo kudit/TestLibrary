66d2757232bad3e009ba220465e203f6978a71d4
kudit <support@kudit.com>
2024-04-06 14:59:38 -0400

    v1.2

--- Sources/TestLibrary/TestLibrary.swift
+++ Sources/TestLibrary/TestLibrary.swift
@@ -3,17 +3,17 @@
 
 import Foundation
 
-extension String {
+public extension String {
     static var testLibrary: String {
         "The test library \(String(describing: Bundle.module)) exists!"
     }
 }
 
-class TestLibrary {
+public class TestLibrary {
     // This is here so we can pull bundle module
 }
 
-extension Bundle {
+public extension Bundle {
     static var testBundleModule = Bundle.module
     static var testLibrary = Bundle(for: TestLibrary.self)
 }
