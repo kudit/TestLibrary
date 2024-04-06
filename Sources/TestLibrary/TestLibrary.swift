223f5888467bff751835d8d8ed4a1589f93c64c6
kudit <support@kudit.com>
2024-04-06 14:58:22 -0400

    v1.1

--- Sources/TestLibrary/TestLibrary.swift
+++ Sources/TestLibrary/TestLibrary.swift
@@ -8,3 +8,12 @@ extension String {
         "The test library \(String(describing: Bundle.module)) exists!"
     }
 }
+
+class TestLibrary {
+    // This is here so we can pull bundle module
+}
+
+extension Bundle {
+    static var testBundleModule = Bundle.module
+    static var testLibrary = Bundle(for: TestLibrary.self)
+}
