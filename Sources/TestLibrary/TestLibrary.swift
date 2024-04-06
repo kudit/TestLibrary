3b21fce5ba3243a890db4fd7fe6a9acf16196650
kudit <support@kudit.com>
2024-04-06 15:06:39 -0400

    v1.4

--- Sources/TestLibrary/TestLibrary.swift
+++ Sources/TestLibrary/TestLibrary.swift
@@ -2,11 +2,17 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
+import SwiftUI
 
 public extension String {
     static var testLibrary: String {
         "The test library \(String(describing: Bundle.module)) exists!"
     }
+}
+
+@available(iOS 13.0, *)
+public extension Image {
+    static var testImage = Image("test", bundle: Bundle.module)
 }
 
 public class TestLibrary {
